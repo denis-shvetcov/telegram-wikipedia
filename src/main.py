74cc65cdf5f4cc84f741dc84301d84239acdac4e
denis-shvetcov <54980645+denis-shvetcov@users.noreply.github.com>
2022-12-28 23:22:18 +0300

    Bot can now respond to incoming messages #1

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
+import re
 import telebot
-
 
 # Token from @BotFather
 API_TOKEN = 'YOUR_TOKEN'
+
+
+def wikiparse(page):
+    wikitext = page.content[:1000]  # message length limit
+    wikimas = wikitext.split('.')
+    wikimas = wikimas[:-1]
+    wikitext2 = ''
+    for x in wikimas:
+        if not ('==' in x):
+            if len((x.strip())) > 3:
+                wikitext2 = wikitext2 + x + '.'
+        else:
+            break
+    # Now, using regular expressions, we remove the markup
+    wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+    wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+    wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
+    # Returning a text string
+    return wikitext2
+
 
 if __name__ == "__main__":
     # Creating the bot
@@ -23,6 +43,15 @@ if __name__ == "__main__":
                          "\n\n<b>Available commands:</b>"
                          "\n/start - initialize the bot".format(message.from_user, bot.get_me()),
                          parse_mode='html')
+
+
+    # Handling incoming messages
+    @bot.message_handler(content_types=['text'])
+    def get_text_messages(message):
+        chat_id = message.chat.id  # Getting id of the chat
+        msg = "Wiki response"
+        bot.send_message(chat_id, msg)
+
 
     # Start
     bot.polling(none_stop=True)
